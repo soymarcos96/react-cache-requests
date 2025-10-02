0d774f3b53fd6ea49c8d25f509374e5434f3458e
Marcos Eduardo Winiarski <marcoseduardowiniarski@gmail.com>
2025-10-01 23:36:06 -0300

    refactor: rename context error description

--- src/hooks/useGlobalCache.ts
+++ src/hooks/useGlobalCache.ts
@@ -4,7 +4,7 @@ import { CacheContext } from "../CacheContext";
 export const useGlobalCache = () => {
   const context = useContext(CacheContext);
   if (!context) {
-    throw new Error('useCache needs to be inside CacheProvider!');
+    throw new Error('useGlobalCache needs to be inside CacheProvider!');
   }
   return context;
 };
